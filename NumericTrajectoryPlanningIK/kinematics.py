Fix of numericalKinematic solver to meters

NumericTrajectoryPlanningIK/kinematics.py
@@ -25,9 +25,10 @@ def T_from_R_t(R, t):
     return T
 
 
-def forwardKinematicsT(q):
-
+def forwardKinematicsT(q, returnPoints=False):
     q1, q2, q3, q4, q5, q6 = q
+
+    T0 = np.eye(4)
 
     T_base = T_from_R_t(
         Rz(q1),
@@ -59,6 +60,14 @@ def forwardKinematicsT(q):
         np.array([98.9, 0, 0])
     )
 
-    T = T_base @ T_shoulder @ T_elbow @ T_forearm @ T_wrist @ T_ee
-    return T
+    Ts = [T0]
+    T = T0
+    for Ti in (T_base, T_shoulder, T_elbow, T_forearm, T_wrist, T_ee):
+        T = T @ Ti
+        Ts.append(T)
 
+    if not returnPoints:
+        return Ts[-1]
+
+    pts = np.array([Ti[:3, 3] for Ti in Ts])
+    return Ts[-1], pts

NumericTrajectoryPlanningIK/main.py
@@ -20,8 +20,8 @@ goal = np.array([-400, -400, 400, 0.5, 0.5, 0.5], float)
 targets = np.linspace(start, goal, N)
 
 traj = generateTrajectoryPose(qStart, targets, jointLimits,
-                               smoothw=2e-3, maxIters=200, tol=1e-6,
-                               damping=1e-4, stepScale=0.5)
+                               smoothw=2e-3, maxIters=400, tol=1e-6,
+                               damping=1e-3, stepScale=0.5)
 
 traj = np.array([clampQ(q, jointLimits)[0] for q in traj])
 

NumericTrajectoryPlanningIK/planner.py
@@ -30,7 +30,7 @@ def poseError(q, targets, wPos=1.0, wRot=0.0001):
     pCur, RCur = T[:3, 3], T[:3, :3]
     pDes = targets[:3]
     RDes = vectorToR(targets[3:])
-    ePos = (pDes - pCur)/1000
+    ePos = (pDes - pCur)
     eRot = RToVector(RDes @ RCur.T)
     return np.hstack([wPos * ePos, wRot * eRot])
 